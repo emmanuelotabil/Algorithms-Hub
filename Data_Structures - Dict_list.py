2a623f1a4cf7ebec6fa0571a918e2091b9cf38db
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-19 09:45:14 +0000

    Data_Structure

--- Data_Structures - Dict_list.py
+++ Data_Structures - Dict_list.py
@@ -1,3 +1,8 @@
 month_expense = {'January':2200,'February':2350,'March':2600,'April':2130,'May':2190}
 month = list(month_expense.keys())
 expense = list(month_expense.values())
+
+print('1. Number of extra dollars spent in February compared to January.')
+
+extra = expense[1]-expense[0]
+print('Extra is ',extra) 
